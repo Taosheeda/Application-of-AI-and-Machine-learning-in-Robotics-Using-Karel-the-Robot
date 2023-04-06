563318f713af9960164a64d600d59882f65dae31
Taofeek Raheem <123008498+Taosheeda@users.noreply.github.com>
2023-04-06 19:46:51 +0200

    Update Mountain.py

--- Mountain.py
+++ Mountain.py
@@ -6,54 +6,44 @@ File: Mountain.py
 Karel climbs a mountain of any size
 and plants a beeper at the top
 """
+
 def main():
     ascend_mountain()
     put_beeper()
     descend_mountain()
 
-# pre: karel is at the top of a mountain!
-# post: karel is on the east side of the mountain
+# defining ascend mountain
 def descend_mountain():
     while front_is_clear():
         step_down()
 
+#Teaching karel how to step down
 def step_down():
     move()
     turn_right()
     move()
     turn_left()
 
-
-# pre: karel is facing a mountain
-# post: karel is on the top of said mountain!!!
+# defining ascend mountain
 def ascend_mountain():
     while front_is_blocked():
         step_up()
 
-def step_up():
+# defining step up
+def step_up():    
     turn_left()
     move()
     turn_right()
     move()
 
-# pre: karel is facing a step of the mountain
-# post: karel is up one step also facing the mountain
-
+# defining turn right
 def turn_right():
     for i in range (3):
         turn_left()
+        
+        
 
 
-
-
-
-
-
-
-
-
-
-
-
+# This is "boilerplate" code which launches your code
 if __name__ == "__main__":
     run_karel_program()
